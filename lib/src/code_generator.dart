44f4d05ec019e7c805c7c7e29abc42d094c84e43
Gegerout <selimchik06@gmail.com>
2024-01-03 20:11:33 +0300

    update

--- lib/src/code_generator.dart
+++ lib/src/code_generator.dart
@@ -4,31 +4,45 @@ import 'models.dart';
 
 class CodeGenerator {
   static Widget generateHomeScreen(HomeScreenConfig config) {
+    final TabController tabController = TabController(
+        length: config.tabs.length, vsync: TestTickerProviderStateMixin());
+
     return Scaffold(
       appBar: AppBar(
         title: Text(config.appBarTitle),
         bottom: TabBar(
-          controller: TabController(
-              length: config.tabs.length,
-              vsync: TestTickerProviderStateMixin()),
+          controller: tabController,
           tabs: config.tabs
               .map((tab) => Tab(icon: Icon(tab.iconData), text: tab.name))
               .toList(),
         ),
       ),
       body: TabBarView(
-        controller: TabController(
-            length: config.tabs.length, vsync: TestTickerProviderStateMixin()),
+        controller: tabController,
         children: config.tabs.map((tab) => generateHomeTab(tab)).toList(),
       ),
     );
   }
 
   static Widget generateHomeTab(HomeTabConfig config) {
-    return Column(
-      children: config.children
-          .map((objectTab) => generateObjectTab(objectTab))
-          .toList(),
+    final TabController tabController = TabController(
+        length: config.children.length, vsync: TestTickerProviderStateMixin());
+
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(80.0),
+        child: TabBar(
+            controller: tabController,
+            tabs: config.children
+                .map((tab) => Tab(
+                      text: tab.name,
+                    ))
+                .toList()),
+      ),
+      body: TabBarView(
+          controller: tabController,
+          children:
+              config.children.map((tab) => generateObjectTab(tab)).toList()),
     );
   }
 
