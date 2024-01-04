aad8f45e0deb2a8a53ba1e5ca3e09ff471ae9d4a
Gegerout <selimchik06@gmail.com>
2024-01-04 15:05:19 +0300

    update

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
-## 0.0.4
+## 0.0.41
 
 Creation of Home tabs and bug fixes
--- lib/src/code_generator.dart
+++ lib/src/code_generator.dart
@@ -2,6 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'models.dart';
 
+class YourStatefulWidget extends StatefulWidget {
+  final ObjectScreenConfig config;
+
+  const YourStatefulWidget({Key? key, required this.config}) : super(key: key);
+
+  @override
+  _YourStatefulWidgetState createState() => _YourStatefulWidgetState();
+}
+
+class _YourStatefulWidgetState extends State<YourStatefulWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return generateObjectScreen(widget.config, () {
+      widget.config.addFunc();
+      setState(() {
+
+      });
+    });
+  }
+}
+
 class CodeGenerator {
   static Widget generateHomeScreen(HomeScreenConfig config) {
     final TabController tabController = TabController(
@@ -49,57 +70,57 @@ class CodeGenerator {
   static Widget generateObjectTab(ObjectTabConfig config) {
     return config.screen;
   }
+}
 
-  static Widget generateObjectScreen(ObjectScreenConfig config) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: [
-                const TextField(
-                  decoration: InputDecoration(labelText: 'Event Type Name'),
-                ),
-                ElevatedButton(
-                  onPressed: config.addFunc,
-                  child: const Text('Add Event Type'),
-                ),
-              ],
-            ),
+Widget generateObjectScreen(ObjectScreenConfig config, VoidCallback onPressedCallback) {
+  return Scaffold(
+    body: Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              const TextField(
+                decoration: InputDecoration(labelText: 'Event Type Name'),
+              ),
+              ElevatedButton(
+                onPressed: onPressedCallback,
+                child: const Text('Add Event Type'),
+              ),
+            ],
           ),
-          FutureBuilder(
-            future: config.loadFunc,
-            builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const CircularProgressIndicator();
-              } else if (snapshot.hasError) {
-                return Text('Error: ${snapshot.error}');
-              } else {
-                List<dynamic> eventTypes = snapshot.data ?? [];
-                return Expanded(
-                  child: ListView.builder(
-                    itemCount: eventTypes.length,
-                    itemBuilder: (context, index) {
-                      return ListTile(
-                        title: Text(eventTypes[index].name),
-                        trailing: IconButton(
-                          icon: const Icon(Icons.delete),
-                          onPressed: () async {
-                            config.deleteFunc();
-                          },
-                        ),
-                      );
-                    },
-                  ),
-                );
-              }
-            },
-          ),
-        ],
-      ),
-    );
-  }
+        ),
+        FutureBuilder(
+          future: config.loadFunc,
+          builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            } else if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            } else {
+              List<dynamic> eventTypes = snapshot.data ?? [];
+              return Expanded(
+                child: ListView.builder(
+                  itemCount: eventTypes.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(eventTypes[index].name),
+                      trailing: IconButton(
+                        icon: const Icon(Icons.delete),
+                        onPressed: () async {
+                          config.deleteFunc();
+                        },
+                      ),
+                    );
+                  },
+                ),
+              );
+            }
+          },
+        ),
+      ],
+    ),
+  );
 }
 
 class TestTickerProviderStateMixin implements TickerProvider {
